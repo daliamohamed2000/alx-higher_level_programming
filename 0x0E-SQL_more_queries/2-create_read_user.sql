8e54298b85bebee015e8c2d086fd0f9f1207cb40
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-03-13 19:16:43 -0700

    a

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,7 @@
 -- creates the database
 CREATE DATABASE IF NOT EXISTS user_0d_2;
 -- creates the user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY
+'user_0d_2_pwd';
 -- grants SELECT privileges to user
 GRANT SELECT ON user_0d_2.* TO user_0d_2@localhost;
