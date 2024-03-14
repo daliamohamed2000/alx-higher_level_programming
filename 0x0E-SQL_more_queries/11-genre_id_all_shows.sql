20f1f93b9d1afd88c2132671f10db40306176c8a
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-03-14 07:25:24 -0700

    a

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
 -- lists all rows of tables in a database
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genresshow_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
