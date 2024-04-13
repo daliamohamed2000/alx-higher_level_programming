061bbb1212739557c8393c2ab384b6fa77ba18d9
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-04-13 09:22:56 -0700

    a

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" script that lists all states from the database """
+"""Start link class to table in database 
+"""
 import sys
-from model_state import Base, state
+from model_state import Base, State
 
-from SQLAlchemy import (create_engine)
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
