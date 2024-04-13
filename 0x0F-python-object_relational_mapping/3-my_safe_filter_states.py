6687898566a40a08f0ff5c463e1eb43bac7deb2d
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-04-13 08:49:40 -0700

    a

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' takes argument and displays values '''
+""" script that lists all states from the database """
 import MySQLdb
 import sys
 
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    match = sys.argv[4]
+    c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = c.fetchall()
     for row in rows:
         print(row)
