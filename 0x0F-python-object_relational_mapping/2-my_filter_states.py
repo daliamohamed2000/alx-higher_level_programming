c1a6dd32cb8947261ff89ee161969f992d3bc1a1
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-04-13 12:23:04 -0700

    a

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" script that lists all states from the database """
-import MySQLdb
-import sys
 
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
-    db.close()
+
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ sessionmaker """
-
-if __name__ == "__main__":
-    
     import sys
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from model_state import State
 
-    if len(sys.argv) < 4:
-        exit(1)
+if __name__ == "__main__":
     #create the sqlalchemy
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     #create a session factor
