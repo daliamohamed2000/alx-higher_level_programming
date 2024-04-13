1659a16ba43bd8ef8c5a49f1cb1d565d672749c3
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-04-13 13:08:24 -0700

    a

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Module that deletes states containing the letter\
-        "a" from a MySQL database using SQLAlchemy."""
+"""Module that deletes states containing the letter "a" from a MySQL database using SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
